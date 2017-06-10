da8a90d6bb0fdc9a56517c11225a667302bac0bf
rieuse <rieuse@163.com>
2017-06-10 23:19:56 +0800

    爬取斗图网站图片

--- scrapystudy/spiders/Scrapy爬虫1：斗图网站最新表情批量抓取.py
+++ scrapystudy/spiders/Scrapy爬虫1：斗图网站最新表情批量抓取.py
@@ -5,13 +5,9 @@ import requests
 class doutu(scrapy.Spider):
     name = "doutu"
     allowed_domains = ["doutula.com","sinaimg.cn"]
-    start_urls = ['https://www.doutula.com/photo/list/?page={}'.format(i) for i in range(1, 8)]
-
-    # start_urls = ['https://www.doutula.com/photo/list/?page=1']
+    start_urls = ['https://www.doutula.com/photo/list/?page={}'.format(i) for i in range(1, 20)]
 
     def parse(self, response):
-        # img_url = response.xpath('//*[@id="pic-detail"]/div/div[1]/div[2]/a/img/@data-original')
-        # name = response.xpath ('//*[@id="pic-detail"]/div/div[1]/div[2]/a/p/text()')
         i = 0
         for content in response.xpath('//*[@id="pic-detail"]/div/div[1]/div[2]/a'):
             i += 1
