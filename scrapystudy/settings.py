c16b46b99fab7921d138a3cc7ebe7e3b7836774a
rieuse <rieuse@163.com>
2017-06-11 21:08:31 +0800

    爬取斗图网站图片

--- scrapystudy/settings.py
+++ scrapystudy/settings.py
@@ -67,13 +67,13 @@ ROBOTSTXT_OBEY = False
 
 # Configure maximum concurrent requests performed by Scrapy (default: 16)
 #default: 16
-CONCURRENT_REQUESTS = 2
+CONCURRENT_REQUESTS = 16
 
 # Configure a delay for requests for the same website (default: 0)
 # See http://scrapy.readthedocs.org/en/latest/topics/settings.html#download-delay
 # See also autothrottle settings and docs
 # DOWNLOAD_DELAY (default: 3)
-DOWNLOAD_DELAY = 7
+DOWNLOAD_DELAY = 0.5
 # The download delay setting will honor only one of:
 #CONCURRENT_REQUESTS_PER_DOMAIN = 16
 #CONCURRENT_REQUESTS_PER_IP = 16
--- scrapystudy/spiders/Scrapy爬虫1：斗图网站最新表情批量抓取.py
+++ scrapystudy/spiders/Scrapy爬虫1：斗图网站最新表情批量抓取.py
@@ -1,11 +1,12 @@
 import scrapy
+import os
 from scrapystudy.items import DoutuItem
 import requests
 
 class doutu(scrapy.Spider):
     name = "doutu"
     allowed_domains = ["doutula.com","sinaimg.cn"]
-    start_urls = ['https://www.doutula.com/photo/list/?page={}'.format(i) for i in range(1, 20)]
+    start_urls = ['https://www.doutula.com/photo/list/?page={}'.format(i) for i in range(20, 60)]
 
     def parse(self, response):
         i = 0
@@ -15,8 +16,10 @@ class doutu(scrapy.Spider):
             item['img_url'] = 'http:' + content.xpath('//img/@data-original').extract()[i]
             item['name'] = content.xpath('//p/text()').extract()[i]
             try:
+                if not os.path.exists('doutu'):
+                    os.makedirs('doutu2')
                 r = requests.get(item['img_url'])
-                filename = 'doutu\\{}'.format(item['name']) + item['img_url'][-4:]
+                filename = 'doutu2\\{}'.format(item['name']) + item['img_url'][-4:]
                 with open(filename, 'wb') as fo:
                     fo.write(r.content)
             except:
