53f1f76a7c147bfd28b1d26c0833f2973c9ed8f2
rieuse <rieuse@163.com>
2017-06-18 08:02:27 +0800

    commit

--- ScrapyXiMaLaYa/ScrapyXiMaLaYa/items.py
+++ ScrapyXiMaLaYa/ScrapyXiMaLaYa/items.py
@@ -6,3 +6,16 @@ class ScrapyximalayaItem(scrapy.Item):
     url = scrapy.Field()
     title = scrapy.Field()
     img_url = scrapy.Field()
+
+
+class ScrapydetaileItem(scrapy.Item):
+    id = scrapy.Field()
+    play_path_64 = scrapy.Field()
+    # play_path_32 = scrapy.Field()
+    # title = scrapy.Field()
+    # nickname = scrapy.Field()
+    # cover_url = scrapy.Field()
+    # formatted_created_at = scrapy.Field()
+    # album_title = scrapy.Field()
+    # intro = scrapy.Field()
+    # category_title = scrapy.Field()
--- ScrapyXiMaLaYa/ScrapyXiMaLaYa/spiders/ximalaya.py
+++ ScrapyXiMaLaYa/ScrapyXiMaLaYa/spiders/ximalaya.py
@@ -1,20 +1,113 @@
 # -*- coding: utf-8 -*-
+import json
+import random
+
 import scrapy
-from bs4 import BeautifulSoup
+from ScrapyXiMaLaYa.items import ScrapydetaileItem
 from ScrapyXiMaLaYa.items import ScrapyximalayaItem
+from bs4 import BeautifulSoup
+from scrapy import Request
 
 
 class XimalayaSpider(scrapy.Spider):
     name = "ximalaya"
     allowed_domains = ["ximalaya.com"]
-    # start_urls = ['http://www.ximalaya.com/dq/all/']
-    start_urls = ['http://www.ximalaya.com/dq/all/{}'.format(num) for num in range(1, 85)]
+    start_urls = ['http://www.ximalaya.com/dq/all/']
+    # start_urls = ['http://www.ximalaya.com/dq/all/{}'.format(num) for num in range(1, 85)]
+    UA_LIST = [
+        "Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.1 (KHTML, like Gecko) Chrome/22.0.1207.1 Safari/537.1",
+        "Mozilla/5.0 (X11; CrOS i686 2268.111.0) AppleWebKit/536.11 (KHTML, like Gecko) Chrome/20.0.1132.57 Safari/536.11",
+        "Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/536.6 (KHTML, like Gecko) Chrome/20.0.1092.0 Safari/536.6",
+        "Mozilla/5.0 (Windows NT 6.2) AppleWebKit/536.6 (KHTML, like Gecko) Chrome/20.0.1090.0 Safari/536.6",
+        "Mozilla/5.0 (Windows NT 6.2; WOW64) AppleWebKit/537.1 (KHTML, like Gecko) Chrome/19.77.34.5 Safari/537.1",
+        "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/536.5 (KHTML, like Gecko) Chrome/19.0.1084.9 Safari/536.5",
+        "Mozilla/5.0 (Windows NT 6.0) AppleWebKit/536.5 (KHTML, like Gecko) Chrome/19.0.1084.36 Safari/536.5",
+        "Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/536.3 (KHTML, like Gecko) Chrome/19.0.1063.0 Safari/536.3",
+        "Mozilla/5.0 (Windows NT 5.1) AppleWebKit/536.3 (KHTML, like Gecko) Chrome/19.0.1063.0 Safari/536.3",
+        "Mozilla/4.0 (compatible; MSIE 7.0; Windows NT 5.1; Trident/4.0; SE 2.X MetaSr 1.0; SE 2.X MetaSr 1.0; .NET CLR 2.0.50727; SE 2.X MetaSr 1.0)",
+        "Mozilla/5.0 (Windows NT 6.2) AppleWebKit/536.3 (KHTML, like Gecko) Chrome/19.0.1062.0 Safari/536.3",
+        "Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/536.3 (KHTML, like Gecko) Chrome/19.0.1062.0 Safari/536.3",
+        "Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/536.3 (KHTML, like Gecko) Chrome/19.0.1061.1 Safari/536.3",
+        "Mozilla/5.0 (Windows NT 6.1) AppleWebKit/536.3 (KHTML, like Gecko) Chrome/19.0.1061.1 Safari/536.3",
+        "Mozilla/5.0 (Windows NT 6.2) AppleWebKit/536.3 (KHTML, like Gecko) Chrome/19.0.1061.0 Safari/536.3",
+        "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/535.24 (KHTML, like Gecko) Chrome/19.0.1055.1 Safari/535.24",
+        "Mozilla/5.0 (Windows NT 6.2; WOW64) AppleWebKit/535.24 (KHTML, like Gecko) Chrome/19.0.1055.1 Safari/535.24",
+        "Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.1 (KHTML, like Gecko) Chrome/22.0.1207.1 Safari/537.1",
+        "Mozilla/5.0 (X11; CrOS i686 2268.111.0) AppleWebKit/536.11 (KHTML, like Gecko) Chrome/20.0.1132.57 Safari/536.11",
+        "Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/536.6 (KHTML, like Gecko) Chrome/20.0.1092.0 Safari/536.6",
+        "Mozilla/5.0 (Windows NT 6.2) AppleWebKit/536.6 (KHTML, like Gecko) Chrome/20.0.1090.0 Safari/536.6",
+        "Mozilla/5.0 (Windows NT 6.2; WOW64) AppleWebKit/537.1 (KHTML, like Gecko) Chrome/19.77.34.5 Safari/537.1",
+        "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/536.5 (KHTML, like Gecko) Chrome/19.0.1084.9 Safari/536.5",
+        "Mozilla/5.0 (Windows NT 6.0) AppleWebKit/536.5 (KHTML, like Gecko) Chrome/19.0.1084.36 Safari/536.5",
+        "Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/536.3 (KHTML, like Gecko) Chrome/19.0.1063.0 Safari/536.3",
+        "Mozilla/5.0 (Windows NT 5.1) AppleWebKit/536.3 (KHTML, like Gecko) Chrome/19.0.1063.0 Safari/536.3",
+        "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_8_0) AppleWebKit/536.3 (KHTML, like Gecko) Chrome/19.0.1063.0 Safari/536.3",
+        "Mozilla/5.0 (Windows NT 6.2) AppleWebKit/536.3 (KHTML, like Gecko) Chrome/19.0.1062.0 Safari/536.3",
+        "Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/536.3 (KHTML, like Gecko) Chrome/19.0.1062.0 Safari/536.3",
+        "Mozilla/5.0 (Windows NT 6.2) AppleWebKit/536.3 (KHTML, like Gecko) Chrome/19.0.1061.1 Safari/536.3",
+        "Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/536.3 (KHTML, like Gecko) Chrome/19.0.1061.1 Safari/536.3",
+        "Mozilla/5.0 (Windows NT 6.1) AppleWebKit/536.3 (KHTML, like Gecko) Chrome/19.0.1061.1 Safari/536.3",
+        "Mozilla/5.0 (Windows NT 6.2) AppleWebKit/536.3 (KHTML, like Gecko) Chrome/19.0.1061.0 Safari/536.3",
+        "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/535.24 (KHTML, like Gecko) Chrome/19.0.1055.1 Safari/535.24",
+        "Mozilla/5.0 (Windows NT 6.2; WOW64) AppleWebKit/535.24 (KHTML, like Gecko) Chrome/19.0.1055.1 Safari/535.24"
+    ]
+    headers1 = {
+        'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8',
+        'Accept-Encoding': 'gzip, deflate, sdch',
+        'Accept-Language': 'zh-CN,zh;q=0.8,en;q=0.6',
+        'Cache-Control': 'max-age=0',
+        'Proxy-Connection': 'keep-alive',
+        'Upgrade-Insecure-Requests': '1',
+        'User-Agent': random.choice(UA_LIST)
+    }
+    headers2 = {
+        'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8',
+        'Accept-Encoding': 'gzip, deflate, sdch',
+        'Accept-Language': 'zh-CN,zh;q=0.8,en;q=0.6',
+        'Cache-Control': 'max-age=0',
+        'Proxy-Connection': 'keep-alive',
+        'Referer': 'http://www.ximalaya.com/dq/all/2',
+        'Upgrade-Insecure-Requests': '1',
+        'User-Agent': random.choice(UA_LIST)
+    }
 
-    def parse(self, response):
-        soup = BeautifulSoup(response.body, 'lxml')
+    def parse(self, respose):
+        soup = BeautifulSoup(respose.body, 'lxml')
         for content in soup.find_all(class_="albumfaceOutter"):
             item = ScrapyximalayaItem()
             item['url'] = content.a['href']
             item['title'] = content.img['alt']
             item['img_url'] = content.img['src']
-            yield item
+            # yield item
+            yield Request(item['url'], headers=self.headers2, callback=self.get_m4a)
+
+    def another(self, respose):
+        item = ScrapyximalayaItem()
+        ifanother = respose.xpath('//div[@class="pagingBar_wrapper"]/a[last()-1]/@data-page')
+        if len(ifanother):
+            num = ifanother[0]
+            for n in range(1, int(num)):
+                url = item['url'] + '?page={}'.format(n)
+                yield Request(url, headers=self.headers2, callback=self.get_m4a)
+            yield Request(item['url'], headers=self.headers2, callback=self.get_m4a)
+
+    def get_m4a(self, respose):
+        numlist = respose.xpath('//div[@class="personal_body"]/@sound_ids')[0].split(',')
+        for i in numlist:
+            murl = 'http://www.ximalaya.com/tracks/{}.json'.format(i)
+            html = Request(murl, headers=self.headers1, callback=self.get_m4a)
+            dic = json.loads(html)
+            item2 = ScrapydetaileItem()
+            item2['id'] = dic['id']
+            item2['play_path_64'] = dic['play_path_64']
+            yield item2
+
+
+            # def parse(self, response):
+            #     soup = BeautifulSoup(response.body, 'lxml')
+            #     for content in soup.find_all(class_="albumfaceOutter"):
+            #         item = ScrapyximalayaItem()
+            #         item['url'] = content.a['href']
+            #         item['title'] = content.img['alt']
+            #         item['img_url'] = content.img['src']
+            #         yield item
